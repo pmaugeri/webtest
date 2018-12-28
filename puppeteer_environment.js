45d29ee31cef818c56bd95b203acac311c142869
Pascal Maugeri <pmaugeri2@gmail.com>
2018-12-28 10:05:00 +0100

    Updated to original Jest custom example adding PuppeteerEnvironment as a module export

--- puppeteer_environment.js
+++ puppeteer_environment.js
@@ -1,37 +1,38 @@
-const chalk = require('chalk')
-const NodeEnvironment = require('jest-environment-node')
-const puppeteer = require('puppeteer')
-const fs = require('fs')
-const os = require('os')
-const path = require('path')
+// puppeteer_environment.js
+const NodeEnvironment = require('jest-environment-node');
+const fs = require('fs');
+const path = require('path');
+const puppeteer = require('puppeteer');
+const os = require('os');
 
-const DIR = path.join(os.tmpdir(), 'jest_puppeteer_global_setup')
+const DIR = path.join(os.tmpdir(), 'jest_puppeteer_global_setup');
 
 class PuppeteerEnvironment extends NodeEnvironment {
   constructor(config) {
-    super(config)
+    super(config);
   }
 
   async setup() {
-    console.log(chalk.yellow('Setup Test Environment.'))
-    await super.setup()
-    const wsEndpoint = fs.readFileSync(path.join(DIR, 'wsEndpoint'), 'utf8')
+    await super.setup();
+    // get the wsEndpoint
+    const wsEndpoint = fs.readFileSync(path.join(DIR, 'wsEndpoint'), 'utf8');
     if (!wsEndpoint) {
-      throw new Error('wsEndpoint not found')
+      throw new Error('wsEndpoint not found');
     }
+
+    // connect to puppeteer
     this.global.__BROWSER__ = await puppeteer.connect({
       browserWSEndpoint: wsEndpoint,
-    })
+    });
   }
 
   async teardown() {
-    console.log(chalk.yellow('Teardown Test Environment.'))
-    await super.teardown()
+    await super.teardown();
   }
 
   runScript(script) {
-    return super.runScript(script)
+    return super.runScript(script);
   }
 }
 
-module.exports = PuppeteerEnvironment
+module.exports = PuppeteerEnvironment;
